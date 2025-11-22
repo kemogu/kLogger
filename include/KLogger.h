a4204ea7d4a5fbb0decc08ab652793bbc11e177f
Kemal Oğuzhan Yeyit <kemaloguzhanyeyit@gmail.com>
2025-11-22 15:42:23 +0000

    ANSI color codes added.

--- include/KLogger.h
+++ include/KLogger.h
@@ -16,6 +16,14 @@ namespace KL {
         ERROR
     };
 
+    namespace Color {
+        // ANSI escape codes.
+        const std::string RED     = "\033[31m";
+        const std::string GREEN   = "\033[32m";
+        const std::string YELLOW  = "\033[33m";
+        const std::string RESET   = "\033[0m";
+    };
+
     class Logger {
         public:
             /**
