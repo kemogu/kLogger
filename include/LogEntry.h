789126d0af3d0fb9dcc77c26737a686ef377c7e4
Kemal Oğuzhan Yeyit <kemaloguzhanyeyit@gmail.com>
2025-11-24 17:47:17 +0000

    time stamp variable added to log entry.

--- include/LogEntry.h
+++ include/LogEntry.h
@@ -1,12 +1,15 @@
 #ifndef LOGENTRY_H
 #define LOGENTRY_H
 
-#include <string>
+#include <string>           // For std::string
+#include <chrono>           // For std::chrono::syttem_clock::time_point
+
 #include "Level.h"
 
 namespace KL {
     struct LogEntry {
         bool writeToFile;
+        std::chrono::system_clock::time_point timeStamp;
         Level level;
         std::string msg;
     };
